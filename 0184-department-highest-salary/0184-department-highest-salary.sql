b8444331169dc3f2a6ae3a69cef0fa19bff41a6a
K.Bhavya Satya Sri <147794361+bhavyasatyasri200@users.noreply.github.com>
2025-02-24 11:05:07 +0530

    Time: 1210 ms (53.75%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,18 +1,17 @@
-# Write your MySQL query statement below
--- select d.name as Department,e.name as Employee,e.salary as Salary
--- from 
--- Employee e
--- join Department d
--- on e.departmentId=d.id
--- having e.salary=(select max(salary ) from Employee group by departmentId);
+#
 
-SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
-FROM Employee e
-JOIN Department d ON e.departmentId = d.id
-WHERE e.salary = (
-    SELECT MAX(salary) 
-    FROM Employee 
-    WHERE departmentId = e.departmentId
-);
-
+-- SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary
+-- FROM Employee e
+-- JOIN Department d ON e.departmentId = d.id
+-- WHERE e.salary = (
+--     SELECT MAX(salary) 
+--     FROM Employee 
+--     WHERE departmentId = e.departmentId
+-- );
+with cte as 
+(select d.name as Department,e.name As Employee,e.salary As Salary,max(salary) over (partition by d.name) as max_salary
+From Employee e
+join 
+Department d on e.departmentId=d.id)
+select Department,Employee,Salary from cte where salary=max_salary;
 
