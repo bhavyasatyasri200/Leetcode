5ba234c0aaba9f5144514c1b2548e789669f7622
K.Bhavya Satya Sri <147794361+bhavyasatyasri200@users.noreply.github.com>
2025-10-12 22:15:37 +0530

    Time: 0 ms (100%), Space: 43.6 MB (18.61%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -2,44 +2,37 @@ class Solution {
     public boolean search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-
         while (left <= right) {
             int mid = (left + right) / 2;
-
             if (nums[mid] == target || nums[left] == target || nums[right] == target) {
                 return true;
             }
-
-            // handle duplicates
             if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
                 left++;
                 right--;
                 continue;
             }
-
-            // if left half is sorted
-            if (nums[left] <= nums[mid]) {
+            else if (nums[left] <= nums[mid]) {
                 int low = left;
                 int high = mid;
-                while (low <= high) { // search in sorted left half
+                while (low <= high) { 
                     int mid1 = (low + high) / 2;
                     if (nums[mid1] == target) return true;
                     else if (nums[mid1] < target) low = mid1 + 1;
                     else high = mid1 - 1;
                 }
-                left = mid + 1; // move to right half after checking left
+                left = mid + 1; 
             } 
-            // if right half is sorted
             else {
                 int low = mid;
                 int high = right;
-                while (low <= high) { // search in sorted right half
+                while (low <= high) { 
                     int mid1 = (low + high) / 2;
                     if (nums[mid1] == target) return true;
                     else if (nums[mid1] < target) low = mid1 + 1;
                     else high = mid1 - 1;
                 }
-                right = mid - 1; // move to left half after checking right
+                right = mid - 1; 
             }
         }
 
