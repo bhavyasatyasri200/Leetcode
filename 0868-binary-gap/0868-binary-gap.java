12bc2967a74a52d85bd5ff6fa7be6eef909d8643
K.Bhavya Satya Sri <147794361+bhavyasatyasri200@users.noreply.github.com>
2026-02-22 17:58:27 +0530

    Time: 0 ms (100%), Space: 42.6 MB (14.06%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int binaryGap(int n) {
+        int maxDistance = 0;
+        int lastPosition = -1;
+        int position = 0;
+        while (n > 0) {
+            if ((n & 1) == 1) { 
+                if (lastPosition != -1) {
+                    maxDistance = Math.max(maxDistance, position - lastPosition);
+                }
+                lastPosition = position;
+            }
+            n >>= 1;  
+            position++;
+        }
+        return maxDistance;
+    }
+}
