15f1bddb78cc27245149e2c25567304bec5997ca
K.Bhavya Satya Sri <147794361+bhavyasatyasri200@users.noreply.github.com>
2026-02-27 06:37:04 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3666-minimum-operations-to-equalize-binary-string/3666-minimum-operations-to-equalize-binary-string.java
+++ 3666-minimum-operations-to-equalize-binary-string/3666-minimum-operations-to-equalize-binary-string.java
@@ -2,21 +2,29 @@ class Solution {
     public int minOperations(String s, int k) {
         int n = s.length();
         int zeros = 0;
+
         for (char c : s.toCharArray()) {
             if (c == '0') zeros++;
         }
+
         if (zeros == 0) return 0;
+
+        // Special case: full flip
         if (k == n) {
-            if (zeros == n) return 1;  
-            else return -1;             
+            if (zeros == n) return 1;
+            else return -1;
         }
-        if (k % 2 == 0 && zeros % 2 == 1) {
+
+        // If k is odd → always possible
+        if (k % 2 == 1) {
+            return zeros;
+        }
+
+        // k is even
+        if (zeros % 2 == 1) {
             return -1;
         }
-        int x = (zeros + k - 1) / k;
-        while ((x * k - zeros) % 2 != 0) {
-            x++;
-        }
-        return x;
+
+        return zeros;
     }
 }
