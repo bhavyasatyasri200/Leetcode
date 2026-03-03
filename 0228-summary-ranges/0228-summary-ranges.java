0d14fa7f53b44776260fdf7b667e78344b63cabf
K.Bhavya Satya Sri <147794361+bhavyasatyasri200@users.noreply.github.com>
2026-03-03 21:07:58 +0530

    Time: 3 ms (78.91%), Space: 42.8 MB (93.44%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int start = nums[i];
+            
+            // Keep moving while the next element is consecutive
+            while (i + 1 < nums.length && nums[i + 1] == nums[i] + 1) {
+                i++;
+            }
+            if (start == nums[i]) {
+                result.add(String.valueOf(start));
+            } else {
+                result.add(start + "->" + nums[i]);
+            }
+        }
+        
+        return result;
+    }
+}
