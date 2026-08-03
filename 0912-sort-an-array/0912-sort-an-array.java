9d87821eedbcc1004bac162f517b5171ac1409be
K BHAVYA SATYA SRI <147794361+bhavyasatyasri200@users.noreply.github.com>
2026-08-03 21:13:42 +0530

    Time: 52 ms (18.89%), Space: 97.5 MB (5.06%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int min=nums[0];
+        int max=nums[0];
+        for(int i=0;i<nums.length;i++){
+            
+                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+           
+            if(nums[i]<min){
+                min=nums[i];
+            }
+            if(nums[i]>max){
+                max=nums[i];
+            }
+        }
+        int index=0;
+        for(int i=min;i<=max;i++){
+            while(map.getOrDefault(i,0) >0){
+                nums[index]=i;
+                index++;
+                map.put(i,map.get(i)-1);
+            }
+        }
+        return nums;
+    }
+}
