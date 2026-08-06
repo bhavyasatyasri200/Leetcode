46816385d400f4ece182cf0d93d126a076e600f5
K BHAVYA SATYA SRI <147794361+bhavyasatyasri200@users.noreply.github.com>
2026-08-06 11:45:38 +0530

    Time: 9 ms (3.59%), Space: 64.1 MB (8.19%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,24 +1,13 @@
-import java.util.*;
-
 class Solution {
     public void rotate(int[] nums, int k) {
-
-        List<Integer> list = new ArrayList<>();
-
-        // copy array to list
-        for (int i = 0; i < nums.length; i++) {
-            list.add(nums[i]);
+        List<Integer> list= new ArrayList<>();
+        for(int num:nums){
+            list.add(num);
         }
-
-        // normalize k
-        k = k % nums.length;
-
-        // rotate
-        Collections.rotate(list, k);
-
-        // copy back
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = list.get(i);
+        k=k%nums.length;
+        Collections.rotate(list,k);
+        for(int i=0;i<nums.length;i++){
+            nums[i]=list.get(i);
         }
     }
 }
