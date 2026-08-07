500c3082c298e0f5eb4ffb8ada3a65ee7d118c5b
K BHAVYA SATYA SRI <147794361+bhavyasatyasri200@users.noreply.github.com>
2026-08-07 06:14:25 +0530

    Time: 2508 ms (5%), Space: 55.4 MB (98.4%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,22 +1,24 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> set=new HashSet<>();
-        int n=nums.length;
-        int target=0;
-        for(int i=0;i<n-2;i++){
-            HashMap<Integer,Integer> map=new HashMap<>();
-            for(int j=i+1;j<n;j++){
-                int complement=target-(nums[i]+nums[j]);
-                if(map.containsKey(complement)){
-                    List<Integer> list=Arrays.asList(
-                        nums[i],nums[j],complement
-                    );
-                    Collections.sort(list);
-                    set.add(list);
+        Set<List<Integer>> ans = new HashSet<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            HashSet<Integer> seen = new HashSet<>();
+
+            for (int j = i + 1; j < n; j++) {
+                int complement = -(nums[i] + nums[j]);
+
+                if (seen.contains(complement)) {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
+                    Collections.sort(triplet);
+                    ans.add(triplet);
                 }
-                map.put(nums[j],j);
+
+                seen.add(nums[j]);
             }
         }
-        return new ArrayList<>(set);
+
+        return new ArrayList<>(ans);
     }
 }
