4a98eaf91894dbb198c529e84678a10a3faef8c4
K BHAVYA SATYA SRI <147794361+bhavyasatyasri200@users.noreply.github.com>
2026-08-08 10:33:45 +0530

    Time: 3 ms (86.01%), Space: 43.3 MB (58.24%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,16 +1,19 @@
-import java.util.*;
-class Solution{
-    public boolean isValid(String s){
+class Solution {
+    public boolean isValid(String s) {
         Stack<Character> stack=new Stack<>();
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(c=='('||c=='{'||c=='[') stack.push(c);
+            char ch=s.charAt(i);
+            if(ch=='(' ||ch=='{' || ch=='['){
+                stack.push(ch);
+            }
+            
             else{
-                if(stack.isEmpty()) return false;
-                char top=stack.pop();
-                if((c==')'&&top!='(')||(c=='}'&&top!='{')||(c==']'&&top!='[')) return false;
+                if(stack.isEmpty())
+                return false;
+            char top=stack.pop();
+             if((ch==')' && top!='(') ||(ch=='}' && top!='{') || (ch==']' && top!='[') ) return false;
             }
         }
-        return stack.isEmpty();
+       return stack.isEmpty();
     }
 }
