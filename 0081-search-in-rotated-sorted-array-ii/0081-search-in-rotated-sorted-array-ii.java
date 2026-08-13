490b295691ad9b7cb4f3c5fa39b2943c2f39a677
K BHAVYA SATYA SRI <147794361+bhavyasatyasri200@users.noreply.github.com>
2026-08-13 10:07:21 +0530

    Time: 6 ms (2.2%), Space: 45.9 MB (12.63%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,41 +1,21 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] == target || nums[left] == target || nums[right] == target) {
+        int arr[]=Arrays.copyOf(nums,nums.length);
+        Arrays.sort(arr);
+        int left=0;
+        int right=arr.length-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(arr[mid]==target){
                 return true;
             }
-            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
-                left++;
-                right--;
-                continue;
+            else if(arr[mid]<target){
+                left=mid+1;
             }
-            else if (nums[left] <= nums[mid]) {
-                int low = left;
-                int high = mid;
-                while (low <= high) { 
-                    int mid1 = (low + high) / 2;
-                    if (nums[mid1] == target) return true;
-                    else if (nums[mid1] < target) low = mid1 + 1;
-                    else high = mid1 - 1;
-                }
-                left = mid + 1; 
-            } 
-            else {
-                int low = mid;
-                int high = right;
-                while (low <= high) { 
-                    int mid1 = (low + high) / 2;
-                    if (nums[mid1] == target) return true;
-                    else if (nums[mid1] < target) low = mid1 + 1;
-                    else high = mid1 - 1;
-                }
-                right = mid - 1; 
+            else{
+                right=mid-1;
             }
         }
-
         return false;
     }
 }
