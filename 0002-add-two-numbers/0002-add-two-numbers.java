7c1d6d13623e83ffdc7619b228dd4f71c867d1cb
K BHAVYA SATYA SRI <147794361+bhavyasatyasri200@users.noreply.github.com>
2026-08-16 18:46:24 +0530

    Time: 1 ms (100%), Space: 46.3 MB (77.86%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,25 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        // Dummy node to simplify result list creation
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-        int carry = 0;
-
-        // Traverse while there are digits left or a carry remains
-        while (l1 != null || l2 != null || carry != 0) {
-            int val1 = (l1 != null) ? l1.val : 0; // value from l1
-            int val2 = (l2 != null) ? l2.val : 0; // value from l2
-
-            int sum = val1 + val2 + carry; // add both digits + carry
-            carry = sum / 10;              // update carry
-            current.next = new ListNode(sum % 10); // create new node
-            current = current.next;        // move forward
-
-            // Move l1 and l2 pointers if possible
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
+        ListNode dummy=new ListNode(0);
+        ListNode ptr=dummy;
+        int carry=0;
+        while(l1!=null || l2!=null){
+            int sum=0+carry;
+            if(l1!=null){
+                sum+=l1.val;
+                l1=l1.next;
+            }
+            if(l2!=null){
+                sum+=l2.val;
+                l2=l2.next;
+            }
+            carry=sum/10;
+            sum=sum%10;
+            ptr.next=new ListNode(sum);
+            ptr=ptr.next;
         }
-
-        return dummy.next; // skip dummy and return result head
+        if(carry==1) {
+            ptr.next=new ListNode(1);
+        }
+        return dummy.next;
     }
 }
