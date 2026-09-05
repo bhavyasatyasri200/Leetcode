c5db6904aa6ba7c40a450467c04e8227317840e2
K BHAVYA SATYA SRI <147794361+bhavyasatyasri200@users.noreply.github.com>
2026-09-05 10:01:11 +0530

    Time: 1 ms (99.79%), Space: 46.3 MB (49.68%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+        int n=nums.length;
+        int maxSum[]=new int[n];
+        int minSum[]=new int[n];
+        maxSum[0]=nums[0];
+        minSum[n-1]=nums[n-1];
+        for(int i=1;i<n;i++){
+            maxSum[i]=Math.max(nums[i],maxSum[i-1]);
+        }
+        for(int i=n-2;i>=0;i--){
+            minSum[i]=Math.min(nums[i],minSum[i+1]);
+        }
+        for(int i=0;i<n;i++){
+            if((maxSum[i]-minSum[i])<=k){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
