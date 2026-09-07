45b57ff635df4ed1bbfd44c37c6c4b76608c88e5
K BHAVYA SATYA SRI <147794361+bhavyasatyasri200@users.noreply.github.com>
2026-09-07 13:58:49 +0530

    Time: 19 ms (49.76%), Space: 98 MB (65.26%) - LeetHub

---
+++ 0077-combinations/0077-combinations.java
@@ -0,0 +1,27 @@
+class Solution {
+    static void solve(int ind,int n,int k,int[] res,List<Integer> list,List<List<Integer>> ans){
+        
+        if(list.size()==k){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        if(ind==n) return;
+        for(int i=ind;i<n;i++){
+            list.add(res[i]);
+            solve(i+1,n,k,res,list,ans);
+            list.remove(list.size()-1);
+        }
+      
+    }
+    public List<List<Integer>> combine(int n, int k) {
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=i+1;
+        }
+        List<Integer> list=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        solve(0,n,k,arr,list,ans);
+        return ans;
+
+    }
+}
