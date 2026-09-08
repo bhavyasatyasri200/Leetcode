33801e53695fdcb524a80f3e756b56cf2314187e
K BHAVYA SATYA SRI <147794361+bhavyasatyasri200@users.noreply.github.com>
2026-09-08 14:43:29 +0530

    Time: 2 ms (34.9%), Space: 45.7 MB (15.73%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,22 +1,59 @@
+// class Solution {
+//     static void solve(int ind,int n,int[] nums,List<Integer> list,List<List<Integer>> res){
+//         if(list.size()==nums.length){
+//             res.add(new ArrayList<>(list));
+//             return;
+//         }
+       
+//         for(int i=ind;i<nums.length;i++){
+//             if(list.contains(nums[i])) continue;
+//             list.add(nums[i]);
+//             solve(0,n,nums,list,res);
+//             list.remove(list.size()-1);
+//         }
+//     }
+//     public List<List<Integer>> permute(int[] nums) {
+//         List<Integer> list=new ArrayList<>();
+//         List<List<Integer>> res=new ArrayList<>();
+//         int n=nums.length;
+//         solve(0,n,nums,list,res);
+//         return res;
+//     }
+// }
 class Solution {
-    static void solve(int ind,int n,int[] nums,List<Integer> list,List<List<Integer>> res){
-        if(list.size()==nums.length){
+
+    static void solve(int i, int[] nums,
+                      List<Integer> list,
+                      List<List<Integer>> res) {
+
+        if (list.size() == nums.length) {
             res.add(new ArrayList<>(list));
             return;
         }
-       
-        for(int i=ind;i<nums.length;i++){
-            if(list.contains(nums[i])) continue;
-            list.add(nums[i]);
-            solve(0,n,nums,list,res);
-            list.remove(list.size()-1);
+
+        if (i == nums.length) {
+            return;
         }
+
+        // Try nums[i]
+        if (!list.contains(nums[i])) {
+            list.add(nums[i]);
+
+            solve(0, nums, list, res);
+
+            list.remove(list.size() - 1);
+        }
+
+        // Move to the next index
+        solve(i + 1, nums, list, res);
     }
+
     public List<List<Integer>> permute(int[] nums) {
-        List<Integer> list=new ArrayList<>();
-        List<List<Integer>> res=new ArrayList<>();
-        int n=nums.length;
-        solve(0,n,nums,list,res);
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+
+        solve(0, nums, list, res);
+
         return res;
     }
 }
