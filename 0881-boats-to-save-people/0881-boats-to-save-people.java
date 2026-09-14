d94756ed6857b805450c308108dbef4c45549f1e
K BHAVYA SATYA SRI <147794361+bhavyasatyasri200@users.noreply.github.com>
2026-09-14 14:46:29 +0530

    Time: 21 ms (22.66%), Space: 56.4 MB (76.12%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.java
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -1,19 +1,16 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        Arrays.sort(people);
         int left=0;
         int right=people.length-1;
-        int boards=0;
+        int boats=0;
+        Arrays.sort(people);
         while(left<=right){
             if(people[left]+people[right]<=limit){
                 left++;
-                right--;
             }
-            else{
-                right--;
-            }
-            boards++;
+            right--;
+            boats++;
         }
-        return boards;
+        return boats;
     }
 }
